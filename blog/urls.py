5e0ce34110bc0dc012de2ba963812d81e8cff65f
oleg <oleg.karetko@mail.ru>
2020-03-01 21:20:54 +0300

    Добавление CSS и шаблона постов

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #pk это номер ссылки - порядковое целое число наших постов
+    #Параметр post_detail означает, что Django будет искать URL с именем post_detail в файле blog.urls.py
+    #Фрагмент post/<int:pk>/ определяет шаблон URL-адреса. Пример - "post/1"
 ]
