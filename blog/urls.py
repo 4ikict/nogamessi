782bd773bf0dd7c57e95ac133eda549e7ea00724
oleg <oleg.karetko@mail.ru>
2020-03-24 20:30:17 +0300

    Добавление New post, редактирование и сохранение постов - только после авторизации пользователя

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     #pk это номер ссылки - порядковое целое число наших постов
     #Параметр post_detail означает, что Django будет искать URL с именем post_detail в файле blog.urls.py
     #Фрагмент post/<int:pk>/ определяет шаблон URL-адреса. Пример - "post/1"
