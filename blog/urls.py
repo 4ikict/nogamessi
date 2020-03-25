d4e6a313270419a1ffd115d45885cd9c1268dee3
oleg <oleg.karetko@mail.ru>
2020-03-25 19:47:01 +0300

    Создание страницы с черновиками постов, кнопки редактирования, удаления,публикации!

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+
     #pk это номер ссылки - порядковое целое число наших постов
     #Параметр post_detail означает, что Django будет искать URL с именем post_detail в файле blog.urls.py
     #Фрагмент post/<int:pk>/ определяет шаблон URL-адреса. Пример - "post/1"
