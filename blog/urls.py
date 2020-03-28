225678b5622629669a4acc4c26d89a00f54612a3
oleg <oleg.karetko@mail.ru>
2020-03-28 18:04:55 +0300

    Создание модели блога для комментариев

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/<pk>/publish/', views.post_publish, name='post_publish'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 
     #pk это номер ссылки - порядковое целое число наших постов
     #Параметр post_detail означает, что Django будет искать URL с именем post_detail в файле blog.urls.py
